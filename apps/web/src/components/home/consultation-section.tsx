7dd0f0a31de6ac1c4eb4b205b85ff5fd6c8993f6
ocreamer888 <ocreamerdrive@gmail.com>
2026-04-18 21:14:05 -0600

    Change consultation section image and WhatsApp button style

--- apps/web/src/components/home/consultation-section.tsx
+++ apps/web/src/components/home/consultation-section.tsx
@@ -6,13 +6,13 @@ export function ConsultationSection() {
   return (
     <section className="relative z-10 px-4 pb-16 md:px-10 md:pb-24 py-24">
       <div
-        className="mx-auto flex w-full max-w-[1200px] flex-col overflow-hidden rounded-[42px] px-4 md:rounded-[102px] md:px-16 lg:flex-row lg:items-end"
+        className="mx-auto flex w-full max-w-[1200px] border border-[#e0ddcf]/20 flex-col overflow-hidden rounded-[42px] px-4 md:rounded-[102px] md:px-16 pt-16 lg:flex-row lg:items-end"
         style={{
           backgroundImage:
             "linear-gradient(344deg, rgba(33, 64, 29, 0.2) 14.951%, rgba(76, 120, 57, 0.2) 81.354%)",
         }}
       >
-        <div className="flex flex-col items-center justify-end lg:justify-start z-10 gap-4 lg:h-[600px] max-w-[795px] pt-16">
+        <div className="flex flex-col items-center justify-end lg:justify-start z-10 gap-4 lg:h-[600px] max-w-[795px]">
           <h2 className="font-serif text-[52px] leading-[0.9] text-white md:text-[80px] lg:text-[82px] lg:leading-[0.9] xl:text-[80px]">
             Teleconsulta
             <br />
@@ -26,21 +26,21 @@ export function ConsultationSection() {
           </p>
 
           <WhatsAppButton
-            className="flex h-[56px] w-full max-w-[400px] items-center justify-center rounded-[62px] bg-[#408733] text-[15px] text-[#eceadd]"
+            className="flex h-[56px] mt-8 w-full max-w-[500px] border border-[#e0ddcf]/50 items-center justify-center rounded-[62px] bg-[#408733] text-[15px] text-[#eceadd]"
           >
             WhatsApp
           </WhatsAppButton>
         </div>
 
-        <div className="relative flex w-full justify-center lg:flex-1 lg:justify-end lg:-mr-12">
+        <div className="relative flex w-full justify-center lg:flex-1 lg:justify-center">
           <Image
-            src="/doctors-2.png"
-            alt="Doctor"
+            src="/dr-javier-leon-farmaplus-huacas.png"
+            alt="Doctor Javier Leon - FarmaPlus Huacas"
             width={832}
             height={1008}
             loading="lazy"
             quality={100}
-            className="h-auto w-full max-w-[520px] lg:max-w-[600px]"
+            className="h-auto w-full max-w-[400px] lg:max-w-[400px]"
           />
         </div>
       </div>
